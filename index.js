da88b8db53a4917661f356c31ca2d286cb91ad8b
Districts13 <deonis013@mail.ru>
2023-12-03 22:55:01 +0200

    Commit

--- index.js
+++ index.js
@@ -49,38 +49,50 @@
 // </html>
 
 // async тестовое от Рафика
-async function fetchData(url, method = 'GET', headers = {}) {
-  try {
-    const response = await fetch(url, { method, headers });
-
-    if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-}
-
-const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
-fetchData(apiUrl)
-  .then(data => {
-    updateHTML(data);
-  })
-  .catch(error => {
-
-    console.error('Failed to fetch data:', error);
-  });
-function updateHTML(data) {
-    // Шаг 1: Создание элементов HTML и Шаг 4: Обновление HTML
-    const dataContainer = document.getElementById('data-container');
-    // Пример: отображение заголовка и тела поста
-    dataContainer.innerHTML = `<h2>${data.title}</h2><p>${data.body}</p>`;
-}
+// async function fetchData(url, method = 'GET', headers = {}) {
+//   try {
+//     const response = await fetch(url, { method, headers });
+//
+//     if (!response.ok) {
+//       throw new Error(`Network response was not ok: ${response.status}`);
+//     }
+//
+//     const data = await response.json();
+//
+//     return data;
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     throw error;
+//   }
+// }
+//
+// const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+// fetchData(apiUrl)
+//   .then(data => {
+//     updateHTML(data);
+//   })
+//   .catch(error => {
+//
+//     console.error('Failed to fetch data:', error);
+//   });
+// function updateHTML(data) {
+//     // Шаг 1: Создание элементов HTML и Шаг 4: Обновление HTML
+//     const dataContainer = document.getElementById('data-container');
+//     // Пример: отображение заголовка и тела поста
+//     dataContainer.innerHTML = `<h2>${data.title}</h2><p>${data.body}</p>`;
+// }
+// document.getElementById('fetch-button').addEventListener('click', () => {
+//   const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+//
+//   // Вызов функции по нажатию кнопки
+//   fetchData(apiUrl)
+//       .then(data => {
+//         updateHTML(data);
+//       })
+//       .catch(error => {
+//         console.error('Failed to fetch data:', error);
+//       });
+// });
 
 
 // fetch('https://api.github.com/')
@@ -100,29 +112,43 @@ p.className = "user-example";
 p.textContent = `Username: ${user}, email: ${emailUser}`;
 document.body.append(p);
 
+ИЛИ
+    // const dataContainer = document.getElementById('data-container');
+    // // Пример: отображение заголовка и тела поста
+    // dataContainer.innerHTML = `<p>Username: ${user}, email: ${emailUser}</p>`;
+
  */
 
-// async function showUser() {
-//     // запрашиваем JSON с данными пользователей
-//     let response = await fetch('https://jsonplaceholder.typicode.com/users');
-//     let users = await response.json();
-//     let user = users[0].username;
-//     let emailUser = users[0].email;
-//     const dataContainer = document.getElementById('data-container');
-//     // Пример: отображение заголовка и тела поста
-//     dataContainer.innerHTML = `<p>Username: ${user}, email: ${emailUser}</p>`;
-//
-//     // запрашиваем информацию о пользователях
-//     // let githubResponse = await fetch(`https://api.github.com/users`);
-//     // let githubUser = await githubResponse.json();
-//
-//     // отображаем данные
-//     // let p = document.createElement('p');
-//     // p.className = "user-example";
-//     // p.textContent = `Username: ${user}, email: ${emailUser}`;
-//     // document.body.append(p);
-//
-// }
+async function showUser() {
+    // запрашиваем JSON с данными пользователей
+    let responseUsers = await fetch('https://jsonplaceholder.typicode.com/users');
+    let users = await responseUsers.json();
+    let user = users[0].username;
+    let emailUser = users[0].email;
+
+    let responsePosts = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let posts = await responsePosts.json();
+    let postTitle = posts[0].title;
+    let postBody = posts[0].body;
+
+    // отображаем данные
+    let p = document.createElement('p');
+    // p.className = "user-example";
+    // p.textContent = `Username: ${user}, email: ${emailUser}`;
+    // document.body.append(p);
+    // let h3 = document.createElement('h3');
+    // h3.className = 'post-example';
+    // h3.textContent = `${postTitle}`;
+    // document.body.append(h3);
+    const dataContainer = document.getElementById('data-container');
+    // Пример: отображение заголовка и тела поста
+    dataContainer.innerHTML = `<p>Username: ${user}, email: ${emailUser}</p><h3>${postTitle}</h3><p>${postBody}</p>`;
+
+
+    // запрашиваем информацию о пользователях
+    // let githubResponse = await fetch(`https://api.github.com/users`);
+    // let githubUser = await githubResponse.json();
+}
 
 // function updateHTML(data) {
 //     // Шаг 1: Создание элементов HTML и Шаг 4: Обновление HTML
